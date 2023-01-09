88a86725285135587fc9a775816aa7a332831476
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-09 21:39:47 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
 
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = try(aws_vpc.this[0].id, "")
+}
+
+
+output "vpc_subnets" {
+  description = "List of IDs of database subnets"
+  value       = aws_subnet.vpc_subnet.id
+}
